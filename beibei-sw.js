08972df2635bb49c75c803481714b246a2c6916d
mabo26 <ly010696@gmail.com>
2026-07-16 22:19:44 +0800

    Fix: network-first SW, always fetch latest

--- beibei-sw.js
+++ beibei-sw.js
@@ -1,16 +1,13 @@
-const CACHE = 'beibei-v1';
-const FILES = ['/beibei-tracker.html'];
+const CACHE = 'beibei-202607161419';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     ).then(() => self.clients.claim())
   );
 });
@@ -18,11 +15,7 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', e => {
   if (e.request.url.includes('beibei-tracker.html')) {
     e.respondWith(
-      fetch(e.request).then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      }).catch(() => caches.match(e.request))
+      fetch(e.request, {cache: 'no-cache'}).catch(() => caches.match(e.request))
     );
   }
 });
